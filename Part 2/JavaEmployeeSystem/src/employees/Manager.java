e38bfb1ea67bfec22f1cdb2f185b2961b23cccc8
RisingSatalite <saihajmann07@gmail.com>
2025-12-05 18:21:02 -0700

    basic java start

---
+++ Part 2/JavaEmployeeSystem/src/employees/Manager.java
@@ -0,0 +1,15 @@
+package employees;
+
+public class Manager extends fullTimeEmployee {
+	int VacationsDays;
+	String Benefits;
+	int AnnualSalary;
+	
+	public void AddEmployeeInfo() {
+		
+	}
+	
+	public void GetPaid() {
+		
+	}
+}
---
+++ Part 2/JavaEmployeeSystem/src/employees/fullTimeEmployee.java
@@ -0,0 +1,15 @@
+package employees;
+
+public class fullTimeEmployee extends EmployeeInfo {
+	int VacationsDays;
+	String Benefits;
+	int AnnualSalary;
+	
+	public void AddEmployeeInfo() {
+		
+	}
+	
+	public void GetPaid() {
+		
+	}
+}
